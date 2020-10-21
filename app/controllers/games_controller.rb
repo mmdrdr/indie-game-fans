6467f7e709794860e99ebd9bc90f5e9932a5a784
vagrant <vagrant@localhost.localdomain>
2020-10-21 09:37:14 +0000

    ゲーム登録機能

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    @game.save
+    redirect_to games_path
+  end
+
+  def index
+    @games = Game.all
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :introduction)
+  end
+
 end
