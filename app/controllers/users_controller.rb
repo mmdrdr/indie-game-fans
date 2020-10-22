726841ca91d2a985c66ca13e4358885d3fc880c2
vagrant <vagrant@localhost.localdomain>
2020-10-22 02:45:17 +0000

    レイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image_id)
+  end
+
 end
