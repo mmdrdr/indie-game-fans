3bbfbc496bc9f2dc303fa0aad9491998727fea34
vagrant <vagrant@localhost.localdomain>
2020-10-22 15:25:27 +0000

    いいねをログインで条件分け

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,7 @@
 class FavoritesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def create
     game = Game.find(params[:game_id])
     favorite = current_user.favorites.new(game_id: game.id)
