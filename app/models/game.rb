a3da09cb9b4fd50569f313e0c436cae9abbab571
vagrant <vagrant@localhost.localdomain>
2020-10-27 07:01:11 +0000

    バリデーションの設定

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,8 +4,15 @@ class Game < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   belongs_to :user, optional: true
+
   attachment :image
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  validates :title, presence: true
+  validates :introduction, presence: true
+  validates :image, presence: true
+
 end
