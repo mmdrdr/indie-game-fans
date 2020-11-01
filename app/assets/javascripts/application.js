fcb5427543744bef81cf0ab67d3c5a702fb604ba
vagrant <vagrant@localhost.localdomain>
2020-11-01 12:43:03 +0000

    かんたんログイン

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,6 @@
 
 document.addEventListener("turbolinks:load", function() {
   $(function() {
-    $('#slider').slick({
-        dots: true, //スライドの下にドットのナビゲーションを表示
-        autoplay: true, //自動再生
-        autoplaySpeed: 4000, //再生スピード
-    });
+    $('#slider').slick();
   });
 });
