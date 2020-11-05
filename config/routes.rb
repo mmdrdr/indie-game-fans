f2769c74ba7fa52113b136fff195f2755d7effbe
vagrant <vagrant@localhost.localdomain>
2020-11-05 09:22:07 +0000

    レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks' 
   }
   root 'homes#top'
-  post 'guest' => 'homes#guest'
+  devise_scope :user do
+    post 'users/guest_sign_in' => 'users/sessions#new_guest'
+  end
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resource :relationships, only: [:create, :destroy]
     get 'follows' => 'relationships#follower', as: 'follows'
