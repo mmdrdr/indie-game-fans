9cca8ee1b69001b2017a7260cb38fc077eb92fef
vagrant <vagrant@localhost.localdomain>
2020-12-08 03:25:33 +0000

    プレビュー機能の切り分け

---
+++ app/assets/javascripts/preview.js
@@ -0,0 +1,18 @@
+$(function() {
+  // 関数の宣言 //
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      // FileReaderを読み込んでローカルファイルを操作できるようにする //
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#img_prev').attr('src', e.target.result);
+      }
+      /* ファイルをData URIとして読み込む。Data URI=外部ファイルを直接ウェブページに埋め込む手段。 */
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  // 新しい画像をアップロードした際の処理 //
+  $("#post_post_img").change(function() {
+    readURL(this);
+  });
+});
