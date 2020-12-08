89d1105da70db03410d559b54c721a3a514cd401
vagrant <vagrant@localhost.localdomain>
2020-12-08 03:39:12 +0000

    turbolinksによる不具合の修正

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,6 +1,6 @@
-$(function() {
-  let position = $("#about").offset().top;
+document.addEventListener("turbolinks:load", function() {
   $("#about-btn").click(function(){
+      let position = $("#about").offset().top;
       $("html,body").animate({
         scrollTop : position
       });
