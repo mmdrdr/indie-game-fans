4a57b5c541e6591d682a18e9221b2dd97f443bbe
vagrant <vagrant@localhost.localdomain>
2020-12-14 13:05:18 +0000

    let var

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load", function() {
   $("#about-btn").click(function(){
-      let position = $("#about").offset().top;
+      var position = $("#about").offset().top;
       $("html,body").animate({
         scrollTop : position
       });
